bc4934eadac90cc25baa31fdf5149202e2a40463
BeeJet1 <bekzatbaiysh@gmail.com>
2023-11-11 21:50:59 +0600

    first commit

---
+++ practice_nov11.dart
@@ -0,0 +1,56 @@
+import 'dart:io';
+
+import 'package:practice_nov11/practice_nov11.dart' as practice_nov11;
+
+void main() {
+  /*for (int i = 2; i <= 20; i += 2) {
+    print(i); //Task 1
+  }*/
+
+  /*int table = 5;
+  for (int i = 1; i <= 9; i++) {
+    int result = table * i;
+    print('$table * $i = $result');
+  }*/ //Task2
+
+  /*int n =5; {
+    int result = 1;
+    for (int i = 1; i <= 5; i++) {
+      result *= i;
+    }
+    print(result); //Task3
+  }*/
+
+  /*List<int> nums = [10, 5, 8, 3, 12];
+  
+  int min = nums[0];
+  int max = nums[0];
+
+  for(int nums in nums) {
+    if (nums < min) {
+      min = nums;
+    }
+    if (nums > max) {
+      max = nums;
+    }
+  }
+  print('min: $min' );
+  print('max: $max');*/ //Task 4
+
+  /*List<int> values = [10, 20, 30, 40, 50];
+  int sum = 0;
+  for (int values in values) {
+    sum += values;
+  }
+  double average = sum / values.length;
+  print('average: $average');*/ //Task 5
+
+  /*List<int> nums = [2, 3, 4, 2, 5, 2, 6];
+  int sum = 1;
+  for (int i = 1; i < nums.length; i++) {
+    if (nums[i] == 2) {
+      sum++;
+    }
+  }
+  print(sum);*/ //Task 6
+  }
